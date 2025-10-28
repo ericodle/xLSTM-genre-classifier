2653b830a4a98cff8f11386b1aedc08f08b88193
ericodle <504068014080@tutanota.com>
2025-10-28 15:49:04 +0800

    remove duplicate focal loss definition

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -43,29 +43,7 @@ from core.constants import (
 from core.utils import ensure_directory, get_device, set_random_seed, setup_logging
 from models import get_model
 from models.base import BaseModel
-from training.losses import LabelSmoothingCrossEntropyLoss
-
-
-class FocalLoss(nn.Module):
-    """Focal Loss for addressing class imbalance."""
-
-    def __init__(self, alpha=1.0, gamma=2.0, reduction="mean"):
-        super().__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.reduction = reduction
-
-    def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction="none")
-        pt = torch.exp(-ce_loss)
-        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
-
-        if self.reduction == "mean":
-            return focal_loss.mean()
-        elif self.reduction == "sum":
-            return focal_loss.sum()
-        else:
-            return focal_loss
+from training.losses import FocalLoss, LabelSmoothingCrossEntropyLoss
 
 
 class ModelTrainer:
